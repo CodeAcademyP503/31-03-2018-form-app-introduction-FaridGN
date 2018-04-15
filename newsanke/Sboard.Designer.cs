babc4f7f6bdffc6bc17242295e1f9c313b970d3d
TuranHuseynov <turan_151@mail.ru>
2018-04-15 18:38:11 +0400

    snake

---
+++ newsanke/Sboard.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace mysnake
+{
+    partial class Sboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.snake = new System.Windows.Forms.Button();
+            this.food = new System.Windows.Forms.Button();
+            this.myboard = new System.Windows.Forms.PictureBox();
+            this.left = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myboard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snake
+            // 
+            this.snake.Location = new System.Drawing.Point(0, 0);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(20, 20);
+            this.snake.TabIndex = 0;
+            this.snake.Text = "S";
+            this.snake.UseVisualStyleBackColor = true;
+            // 
+            // food
+            // 
+            this.food.Location = new System.Drawing.Point(60, 60);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(20, 20);
+            this.food.TabIndex = 1;
+            this.food.Text = "F";
+            this.food.UseVisualStyleBackColor = true;
+            // 
+            // myboard
+            // 
+            this.myboard.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.myboard.Location = new System.Drawing.Point(0, 0);
+            this.myboard.Name = "myboard";
+            this.myboard.Size = new System.Drawing.Size(320, 320);
+            this.myboard.TabIndex = 2;
+            this.myboard.TabStop = false;
+            this.myboard.Visible = false;
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(326, 319);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(40, 23);
+            this.left.TabIndex = 3;
+            this.left.Text = "Left";
+            this.left.UseVisualStyleBackColor = true;
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(408, 319);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(40, 23);
+            this.right.TabIndex = 4;
+            this.right.Text = "Right";
+            this.right.UseVisualStyleBackColor = true;
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(364, 348);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(44, 23);
+            this.down.TabIndex = 5;
+            this.down.Text = "Down";
+            this.down.UseVisualStyleBackColor = true;
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(364, 290);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(44, 23);
+            this.up.TabIndex = 6;
+            this.up.Text = "Up";
+            this.up.UseVisualStyleBackColor = true;
+            // 
+            // Sboard
+            // 
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(475, 402);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.snake);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.myboard);
+            this.Name = "Sboard";
+            ((System.ComponentModel.ISupportInitialize)(this.myboard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button snake;
+        private System.Windows.Forms.Button food;
+        private System.Windows.Forms.PictureBox myboard;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button up;
+    }
+}
---
+++ newsanke/Sboard.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mysnake
+{
+    public partial class Sboard : Form
+    {
+        public int LeftPos = 0;
+        public int TopPos = 0;
+        public List<Button> slist = new List<Button>();
+        public Sboard()
+        {
+            InitializeComponent();
+            myboard.Width = 320;
+            myboard.Height = 320;
+
+            snake.Height = 20;
+            snake.Width = 20;
+            food.Width = 20;
+            food.Height = 20;
+            snake.Left = LeftPos;
+            snake.Top = TopPos;
+            slist.Add(snake);
+
+            Controls.Add(snake);
+            Controls.Add(left);
+            Controls.Add(right);
+            Controls.Add(up);
+            Controls.Add(down);
+            Controls.Add(food);
+            food.BringToFront();
+            snake.BringToFront();
+
+           
+
+            left.Click += new EventHandler(Moveleft);
+            right.Click += new EventHandler(Moveright);
+            up.Click += new EventHandler(Moveup);
+            down.Click += new EventHandler(Movedown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Moveleft(object sender, EventArgs e)
+        {
+
+
+            snake.Left -= snake.Width;
+
+            if (snake.Left < 0)
+            {
+                snake.Left = myboard.Width - snake.Width;
+            }
+            for (int i = slist.Count-1; i > 0; i--)
+            {
+                slist[i].Left = slist[i - 1].Left + 20;
+                slist[i].Top = slist[i - 1].Top;
+                if (i >= 2)
+                {
+                    slist[i].Left = slist[i - 1].Left;
+                    slist[i].Top = slist[i - 1].Top;
+                }
+            }
+
+            if (snake.Location == food.Location)
+            {
+                Eat();
+            }
+        }
+
+
+        private void Moveup(object sender, EventArgs e)
+        {
+            snake.Top -= snake.Height;
+
+            if (snake.Top < 0)
+            {
+                snake.Top = myboard.Height - snake.Height;
+            }
+            for (int i = slist.Count - 1; i > 0; i--)
+            {
+                slist[i].Left = slist[i - 1].Left + 20;
+                slist[i].Top = slist[i - 1].Top;
+                if (i >= 2)
+                {
+                    slist[i].Left = slist[i - 1].Left;
+                    slist[i].Top = slist[i - 1].Top;
+                }
+            }
+
+            if (snake.Location == food.Location)
+            {
+                Eat();
+            }
+        }
+
+        private void Movedown(object sender, EventArgs e)
+        {
+            snake.Top += snake.Height;
+
+            if (snake.Top > myboard.Height - snake.Height - 20)
+            {
+                snake.Top = -snake.Height;
+            }
+            for (int i = slist.Count - 1; i > 0; i--)
+            {
+                slist[i].Left = slist[i - 1].Left + 20;
+                slist[i].Top = slist[i - 1].Top;
+                if (i >= 2)
+                {
+                    slist[i].Left = slist[i - 1].Left;
+                    slist[i].Top = slist[i - 1].Top;
+                }
+            }
+
+            if (snake.Location == food.Location)
+            {
+                Eat();
+            }
+        }
+
+        private void Moveright(object sender, EventArgs e)
+        {
+            snake.Left += snake.Width;
+
+            if (snake.Left > myboard.Width - snake.Width - 20)
+            {
+                snake.Left = -snake.Width;
+            }
+            for (int i = slist.Count - 1; i > 0; i--)
+            {
+                slist[i].Left = slist[i - 1].Left + 20;
+                slist[i].Top = slist[i - 1].Top;
+                if (i >= 2)
+                {
+                    slist[i].Left = slist[i - 1].Left;
+                    slist[i].Top = slist[i - 1].Top;
+                }
+            }
+
+            if (snake.Location == food.Location)
+            {
+                Eat();
+            }
+        }
+
+
+        public void Generatefood()
+        {
+            int maxLeftPos = myboard.Width - food.Width;
+            int maxTopPos = myboard.Height - food.Height; 
+            Random rand = new Random();  
+            Button body = new Button();
+            body.Left = rand.Next(0, maxLeftPos);
+            body.Top = rand.Next(0, maxTopPos); 
+            body.Width = snake.Width;
+            body.Height = snake.Height;
+            body.BringToFront();
+            slist.Add(body);
+            Controls.Add(body);
+        }
+        public void Eat()
+        {
+            Controls.Remove(food); 
+            Generatefood();  
+        } 
+    }
+}
